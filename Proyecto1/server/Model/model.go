4ef9ea1da1d7e8cf756b9a6931804fb0cd2a7496
PiterValiente <3054635940207@ingenieria.usac.edu.gt>
2024-06-13 20:04:49 -0600

    add database and connecting with server and read RAM

---
+++ Proyecto1/server/Model/model.go
@@ -0,0 +1,9 @@
+package Model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Data struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Percent   string             `json:"percent"`
+	Timestamp primitive.DateTime `json:"timestamp"`
+}
