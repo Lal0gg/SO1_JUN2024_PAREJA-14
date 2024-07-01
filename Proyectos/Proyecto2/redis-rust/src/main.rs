56e38f970dbe2647c10d153d1366329541e93a1d
Lal0g <2993815280101@ingenieria.usac.edu.gt>
2024-06-30 23:39:15 -0600

    working grafana and rust with success just a littel bug with mongo

--- Proyectos/Proyecto2/redis-rust/src/main.rs
+++ Proyectos/Proyecto2/redis-rust/src/main.rs
@@ -1,8 +1,9 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
+use redis::Commands;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
-use redis::Commands;
 
 #[derive(Serialize, Deserialize)]
 struct Data {
@@ -10,24 +11,57 @@ struct Data {
     Texto: String,
 }
 
-#[post("/set",format="json", data = "<data>")]
+#[derive(Serialize, Deserialize)]
+struct CountryCount {
+    Pais: String,
+    Contador: i32,
+}
 
+#[post("/set", format = "json", data = "<data>")]
 async fn set_data(data: Json<Data>) -> Result<&'static str, &'static str> {
     // Crear cliente de redis
-    let client = redis::Client::open("redis://redis:6379/")
-        .map_err(|_| "Failed to create Redis client")?;
+    let client =
+        redis::Client::open("redis://redis:6379/").map_err(|_| "Failed to create Redis client")?;
 
     // Conexion a redis
-    let mut con = client.get_connection()
+    let mut con = client
+        .get_connection()
         .map_err(|_| "Failed to connect to Redis")?;
 
-    // Insertar hash en redis
-    let _: () = con.hincr(&data.Pais, &data.Texto, 1)
+    // Insertar en redis
+    let _: () = con
+        .hincr(&data.Pais, &data.Texto, 1)
         .map_err(|_| "Failed to set data in Redis")?;
+
     Ok("Data set")
+}
+
+#[post("/country_count", format = "json", data = "<country_count>")]
+async fn set_country_count(
+    country_count: Json<CountryCount>,
+) -> Result<&'static str, &'static str> {
+    // Crear cliente de redis
+    let client =
+        redis::Client::open("redis://redis:6379/").map_err(|_| "Failed to create Redis client")?;
+
+    // Conexion a redis
+    let mut con = client
+        .get_connection()
+        .map_err(|_| "Failed to connect to Redis")?;
+
+    // Insertar en redis
+    let _: () = con
+        .hset(
+            "country_counts",
+            &country_count.Pais,
+            country_count.Contador,
+        )
+        .map_err(|_| "Failed to set country count in Redis")?;
+
+    Ok("Country count set")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![set_data])
+    rocket::build().mount("/", routes![set_data, set_country_count])
 }
